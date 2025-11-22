2277368ab7ee53f7e54549a7f890dfa30eed25bd
Alexis <alexisnomain@icloud.com>
2025-11-22 14:28:14 -0400

    feat: add hero section
    I'm working on a style based on Apple.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,27 @@ function App() {
   return (
     <>
     <Header />
+    <main>
+      <section className='hero'>
+        <div className='gradient'>
+          <div className='container'>
+            <div className='status'>
+              <span className='label'>Disponible para proyectos</span>
+              <div className='signal'></div>
+            </div>
+            <h1>Diseño que <span>inspira.</span></h1>
+            <p>
+              Desarrollador Full Stack & Diseñador UI/UX. 
+              Creo experiencias digitales extraordinarias que combinan tecnología con diseño.
+            </p>
+            <div className='action-buttons'>
+              <a href="./" className='like-button fill'>Ver proyectos</a>
+              <a href="./" className='like-button'>Contactar</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
     </>
   );
 }
